39818d88d1607db6c49cdff3cef87bf61961beaf
normal-pcer <normal-pcer@outlook.com>
2024-09-23 22:30:02 +0800

    1

---
+++ 146.cpp
@@ -0,0 +1,13 @@
+#include "lib"
+
+const int _N = 505, _M = 6005;
+int N = read(), M = read();
+int V[_N], W[_N], S[_N];
+
+int main() {
+    initDebug;
+    std::cout << M+N << std::endl;
+    return 0;
+}
+
+int L = read();
