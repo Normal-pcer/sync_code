87174067eaefc949d08a7aaf09eee82162f0e23e
normal-pcer <normal-pcer@outlook.com>
2024-10-14 22:25:00 +0800

    1

---
+++ __SegmentTree.cpp
@@ -0,0 +1,165 @@
+#include <bits/stdc++.h>
+#define initDebug DEBUG_MODE=(argc-1)&&!strcmp("-d", argv[1])
+#define debug if(DEBUG_MODE)
+#define log(f, a...) debug printf(f, ##a);
+#define upto(i,n) for(int i=1;i<=(n);i++)
+#define from(i,b,e) for(int i=(b);i<=(e);i++)
+#define reverse(i,e,b) for(int i=(e);i>=(b);i--)
+#define main() main(int argc, char const *argv[])
+#define optimizeIO std::ios::sync_with_stdio(false); std::cin.tie(0); std::cout.tie(0);
+#define chkMax(base,cmp) base=std::max((base),(cmp))
+#define chkMin(base,cmp) base=std::min((base),(cmp))
+#define update(base,op,modify) base=op((base),(modify))
+#define ensure(con, otw) ((con)? (con): (otw))
+#define check(v, con, otw) (((v) con)? v: otw)
+#define optional(ptr) if(ptr)ptr
+#define never if(0)
+#define always if(1)
+bool DEBUG_MODE=false;
+typedef long long ll; typedef unsigned long long ull; typedef __int128 lll;
+inline void batchOutput(int **begin, int rows, int cols, const char *format){upto(i, rows){upto(j, cols)printf(format, begin[i][j]);printf("\n");}} inline void batchOutput(int**begin, int rows, int cols) {batchOutput(begin,rows,cols,"%3d");}
+template <class T=int>inline T read() { T x=0;int f=1;char c;while((c=getchar())<'0'||c>'9')if(c=='-')f=-1;do{x=(((x<<2)+x)<<1)+c-'0';}while((c=getchar())>='0'&&c<='9');return x*f; }
+
+const int maxN = (int)1e5+5;
+
+template <
+    const int sz,
+    class dat_t=int, 
+    class tag_t=dat_t
+>
+class SegmentTree {
+public:
+    int ls(int p) { return p<<1; }
+    int rs(int p) { return p<<1|1; }
+    struct Node {
+        int l, r;
+        dat_t data;
+        tag_t tag;
+    } tr[sz];
+    const std::function<void(Node&,Node&,Node&)> pushUpCallback;
+    const std::function<void(Node&,Node&)> pushDownCallback;
+    const dat_t emptyData;
+    const tag_t emptyTag;
+    template <class T, class F1, class F2>
+    SegmentTree(
+        F1&& upCallback,
+        F2&& downCallback,
+        dat_t&& emptyData = 0,
+        tag_t&& emptyTag = 0
+    ): pushUpCallback(upCallback), pushDownCallback(downCallback), 
+        emptyData(emptyData), emptyTag(emptyTag)
+    {
+        build(1, 0, sz-1);
+    }
+
+    void pushUp(int p) {
+        pushUpCallback(tr[p], tr[ls(p)], tr[rs(p)]);
+    }
+    void pushDown(int p) {
+        pushDownCallback(tr[p], tr[ls(p)]);
+        pushDownCallback(tr[p], tr[rs(p)]);
+    }
+
+    template <class initializer_t>
+    void build(int p, int l, int r, initializer_t&& initializer) {
+        tr[p].l = l, tr[p].r = r;
+        if (l==r) {
+            tr[p].data = initializer(l);
+            return;
+        }
+        int m = (l+r) >> 1;
+        build((p<<1), l, m);
+        build((p<<1|1), m+1, r);
+    }
+
+    void build(int p, int l, int r) {
+        tr[p].l = l, tr[p].r = r;
+        if (l==r) {
+            return;
+        }
+        int m = (l+r) >> 1;
+        build((p<<1), l, m);
+        build((p<<1|1), m+1, r);
+    }
+
+    dat_t query(int p, int l, int r) {
+        if (tr[p].l >= l && tr[p].r <= r)  return tr[p].data;
+        pushDown(p);
+        dat_t res = emptyData;
+        if (tr[ls(p)].r >= l)   res = pushUpCallback(res, res, query(ls(p), l, r));
+        if (tr[rs(p)].l <= r)   res = pushUpCallback(res, res, query(rs(p), l, r));
+        return res;
+    }
+};
+
+int N, M;
+ll init[maxN];
+
+struct Node {
+        int l, r, len;
+        ll sum=0;  // 区间内求和
+        ll tag=0;  // 需要下传的更改
+    } tr[maxN<<2];
+
+
+void pushUp(int i) {  // 向上合并信息
+    tr[i].sum = tr[(i<<1)].sum + tr[(i<<1|1)].sum;
+}
+
+void pushDown(int i) {  // 向下推送更改
+    if (tr[i].tag){
+        tr[(i<<1)].sum += tr[i].tag * tr[(i<<1)].len;
+        tr[(i<<1|1)].sum += tr[i].tag * tr[(i<<1|1)].len;
+        tr[(i<<1)].tag += tr[i].tag;
+        tr[(i<<1|1)].tag += tr[i].tag;
+        tr[i].tag = 0;
+    }
+}
+
+void build(int p, int l, int r) {
+    tr[p].l = l, tr[p].r = r, tr[p].len = r-l+1;
+    if (l==r) { tr[p].sum = init[l]; return; }
+    int m = (l+r) >> 1;
+    build((p<<1), l, m);
+    build((p<<1|1), m+1, r);
+    pushUp(p);
+}
+
+ll sum(int p, int l, int r) {
+    if (tr[p].l >= l && tr[p].r <= r)  return tr[p].sum;
+    pushDown(p);
+    ll res = 0;
+    if (tr[(p<<1)].r >= l)   res += sum((p<<1), l, r);
+    if (tr[(p<<1|1)].l <= r)   res += sum((p<<1|1), l, r);
+    return res;
+}
+
+void add(int p, int l, int r, int val) {
+    if (tr[p].l >= l && tr[p].r <= r)  { tr[p].sum += val * tr[p].len;tr[p].tag += val; return; }
+    pushDown(p);
+    if (tr[(p<<1)].r >= l)   add((p<<1), l, r, val);
+    if (tr[(p<<1|1)].l <= r)   add((p<<1|1), l, r, val);
+    pushUp(p);
+}
+
+int main() {
+    initDebug;
+
+    scanf("%d%d", &N, &M);
+    upto(i, N)  scanf("%lld", init+i);
+
+    build(1, 1, N);
+
+    int OP, x, y, k;
+    upto(_, M) {
+        scanf("%d", &OP);
+        if (OP == 1) {  // 加k
+            scanf("%d%d%d", &x, &y, &k);
+            add(1, x, y, k);
+        } else {  // 查询
+            scanf("%d%d", &x, &y);
+            printf("%lld\n", sum(1, x, y));
+        }
+    }
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,114 +1,229 @@
+/**
+ * 
+ */
+ 
 #include <bits/stdc++.h>
 #define initDebug DEBUG_MODE=(argc-1)&&!strcmp("-d", argv[1])
 #define debug if(DEBUG_MODE)
 #define log(f, a...) debug printf(f, ##a);
 #define upto(i,n) for(int i=1;i<=(n);i++)
 #define from(i,b,e) for(int i=(b);i<=(e);i++)
-#define reverse(i,e,b) for(int i=(e);i>=(b);i--)
+#define rev(i,e,b) for(int i=(e);i>=(b);i--)
 #define main() main(int argc, char const *argv[])
 #define optimizeIO std::ios::sync_with_stdio(false); std::cin.tie(0); std::cout.tie(0);
-#define chkMax(base,cmp) base=std::max((base),(cmp))
-#define chkMin(base,cmp) base=std::min((base),(cmp))
-#define update(base,op,modify) base=op((base),(modify))
-#define ensure(con, otw) ((con)? (con): (otw))
-#define check(v, con, otw) (((v) con)? v: otw)
-#define optional(ptr) if(ptr)ptr
-#define never if(0)
-#define always if(1)
+#define chkMax(base,cmp...) (base=std::max({(base),##cmp}))
+#define chkMin(base,cmp...) (base=std::min({(base),##cmp}))
+#define chkMaxEx(base,exchange,other,cmp...) {auto __b__=base;if(__b__!=chkMax(base,##cmp)){exchange;} else other;}
+#define chkMinEx(base,exchange,other,cmp...) {auto __b__=base;if(__b__!=chkMin(base,##cmp)){exchange;} else other;}
+#define ensure(v, con, otw) (((v) con)? (v): (otw))
+#define never if constexpr(0)
+#define always if constexpr(1)
+#define bitOr(x,y) (((x)&(y))^(((x)^(y))|(~(x)&(y))))
+#define Infinity 2147483647
+#define compare(x,y,g,e,l) (((x)>(y))?(g):(((x)<(y))?(l):(e)))
 bool DEBUG_MODE=false;
-typedef long long ll; typedef unsigned long long ull; typedef __int128 lll;
-inline void batchOutput(int **begin, int rows, int cols, const char *format){upto(i, rows){upto(j, cols)printf(format, begin[i][j]);printf("\n");}} inline void batchOutput(int**begin, int rows, int cols) {batchOutput(begin,rows,cols,"%3d");}
-template <class T=int>inline T read() { T x=0;int f=1;char c;while((c=getchar())<'0'||c>'9')if(c=='-')f=-1;do{x=(((x<<2)+x)<<1)+c-'0';}while((c=getchar())>='0'&&c<='9');return x*f; }
-
-#define ls(i) (i<<1)
-#define rs(i) (i<<1|1)
-
-const int maxN = (int)1e5+5;
-
-template <class T, const int sz>
-class SegmentTree {
-public:
-    const std::function<void(int)> pushUpCallback;
-    SegmentTree(std::function<void(int)> callback) pushUpCallback(callback) {}
-    template <const int grp=1>
-    struct Node {
-        int l, r, len;
-        T data[grp];
-        T tag[grp];
-    } tr[maxN<<2];
-};
-
-int N, M;
-ll init[maxN];
-
-struct Node {
-        int l, r, len;
-        ll sum=0;  // 区间内求和
-        ll tag=0;  // 需要下传的更改
-    } tr[maxN<<2];
-
-
-void pushUp(int i) {  // 向上合并信息
-    tr[i].sum = tr[ls(i)].sum + tr[rs(i)].sum;
+typedef long long ll; typedef unsigned long long ull;
+inline void batchOutput(int *begin, int n, const char *format){upto(i, n)printf(format, begin[i]);printf("\n");} inline void batchOutput(int*begin, int n) {batchOutput(begin,n,"%3d ");}
+#define batchOutput2d(b, r, c, fmt) upto(i,r){upto(j,c)printf(fmt,b[i][j]);printf("\n");}
+namespace lib{}
+ 
+#include <bits/stdc++.h>
+namespace lib{
+    // template <const size_t MAXSIZE>
+    struct IO {
+        // char buf[MAXSIZE],*p1,*p2;
+        // char pbuf[MAXSIZE],*pp;
+        // inline char gc() {
+        //  if (p1==p2) p2=(p1=buf)+fread(buf,1,MAXSIZE,stdin);
+        //  return p1==p2?' ':*p1++;
+        // }
+        inline char gc() {
+            return getchar();
+        }
+        inline bool blank(char ch) { return ch==' ' or ch=='\n' or ch=='\r' or ch=='\t'; }
+        inline bool isd(char x) {return (x>='0' and x<='9');}
+        std::string input() {
+            char c = gc();
+            std::string res="";
+            while((c&&!blank(c)) || c==' ') {
+                res.push_back(c);
+                c = gc();
+            }
+            return res;
+        }
+        template <class T>
+        inline void scan(T &x) {
+            double tmp=1;
+            bool sign=0;
+            x=0;
+            char ch=gc();
+            for (; not isd(ch); ch=gc()) if (ch=='-') sign=1;
+            for (; isd(ch); ch=gc()) x=x*10+(ch^48);
+            if (ch=='.') for (ch=gc(); isd(ch); ch=gc()) tmp*=.1f,x+=tmp*(ch^48);
+            if (sign) x=-x;
+        }
+        inline void scan(char *s) {
+            char ch=gc();
+            for (; blank(ch); ch=gc());
+            for (; not blank(ch); ch=gc()) *s++=ch;
+            *s=0;
+        }
+        inline void scan(char &c) {
+            for (c=gc(); blank(c); c=gc());
+        }
+        inline void scan(std::string &s){
+            std::string().swap(s);
+            char ch=gc();
+            for (; blank(ch); ch=gc());
+            for (; not blank(ch); ch=gc()) s.push_back(ch);
+        }
+        template <class T,class... Types>
+        inline void scan(T &x,Types &...args){
+            scan(x);
+            scan(args...);
+        }
+        inline void push(const char &c) {
+            // if (pp-pbuf==MAXSIZE) fwrite(pbuf,1,MAXSIZE,stdout),pp=pbuf;
+            // *pp++=c;
+            putchar(c);
+        }
+        template <class T>
+        inline void write(T x) {
+            if (x<0) x=-x,push('-');
+            static char sta[40];
+            int top=0;
+            do {
+                sta[top++]=x%10^48,x/=10;
+            } while (x);
+            while (top) push(sta[--top]);
+        }
+        inline void write(double x) {
+            printf("%lf",x);
+        }
+        inline void write(float x) {
+            printf("%f",x);
+        }
+        inline void write(char c) {
+            push(c);
+        }
+        inline void write(std::string &s){
+            for (auto i:s) push(i);
+        }
+        inline void write(const char *s){
+            for (; *s; ++s) push(*s);
+        }
+        template <class T,class... Types>
+        inline void write(T &x,Types &...args){
+            write(x);
+            write(args...);
+        }
+ 
+        template<class T=int>
+        inline T read() {
+            T x;
+            scan(x);
+            return x;
+        }
+ 
+        // 流式输入输出
+        template <class T>
+        inline IO& operator>>(T&x) {
+            scan(x);
+            return *this;
+        }
+        template <class T>
+        inline IO& operator<<(T&x) {
+            write(x);
+            return *this;
+        }
+    };
+    IO io;
+    const char endl = '\n';
 }
-
-void pushDown(int i) {  // 向下推送更改
-    if (tr[i].tag){
-        tr[ls(i)].sum += tr[i].tag * tr[ls(i)].len;
-        tr[rs(i)].sum += tr[i].tag * tr[rs(i)].len;
-        tr[ls(i)].tag += tr[i].tag;
-        tr[rs(i)].tag += tr[i].tag;
-        tr[i].tag = 0;
+namespace lib {
+    namespace binary {  // 二分
+        template <class T, class Func>
+        T lower_bound_mapping(
+            T begin,
+            T end,
+            T val,
+            Func &&mapping,
+            T eps = 1
+        ) {
+            while (end-begin >= eps) {
+                T mid = begin + (end-begin)/2;
+                if (mapping(mid) < val) {
+                    begin = mid + eps;
+                } else {
+                    end = mid;
+                }
+            }
+            return begin;
+        }
+ 
+        template <class T, class Func>
+        T upper_bound_mapping(
+            T begin,
+            T end,
+            T val,
+            Func &&mapping,
+            T eps = 1
+        ) {
+            while (end-begin >= eps) {
+                T mid = begin + (end-begin)/2;
+                if (mapping(mid) <= val) {
+                    begin = mid + eps;
+                } else {
+                    end = mid;
+                }
+            }
+            return begin;
+        }
+    }
+}
+using namespace lib;
+ 
+ 
+namespace Solution {
+ 
+    int A, B;
+    const int _A = 1e5+5, _V = 1e9+5;
+    int p[_A];
+ 
+    void init() {
+        io >> A >> B;
+        from(i, 0, A-1)
+            io >> p[i];
+    }
+ 
+    void solve() {
+        init();
+        // 二分最长间距，返回节点数量的最小值的相反数
+        int res = binary::lower_bound_mapping(0, _V, -B, [](int dis) {
+            log("%d -> ", dis);
+            int cnt = 1;
+            int d = 0;
+            for (int i=1; i<A; i++) {
+                int d_i = p[i]-p[i-1];
+                if (d_i > dis) {
+                    return -Infinity;
+                }
+                d += d_i;
+                if (d >= dis) {
+                    d = dis;
+                    cnt++;
+                }
+            }
+            log("%d\n", cnt);
+            return -cnt;
+        });
+        printf("%d\n", res-1);
     }
 }
 
-void build(int p, int l, int r) {
-    tr[p].l = l, tr[p].r = r, tr[p].len = r-l+1;
-    if (l==r) { tr[p].sum = init[l]; return; }
-    int m = (l+r) >> 1;
-    build(ls(p), l, m);
-    build(rs(p), m+1, r);
-    pushUp(p);
-}
-
-ll sum(int p, int l, int r) {
-    if (tr[p].l >= l && tr[p].r <= r)  return tr[p].sum;
-    pushDown(p);
-    ll res = 0;
-    if (tr[ls(p)].r >= l)   res += sum(ls(p), l, r);
-    if (tr[rs(p)].l <= r)   res += sum(rs(p), l, r);
-    return res;
-}
-
-void add(int p, int l, int r, int val) {
-    if (tr[p].l >= l && tr[p].r <= r)  { tr[p].sum += val * tr[p].len;tr[p].tag += val; return; }
-    pushDown(p);
-    if (tr[ls(p)].r >= l)   add(ls(p), l, r, val);
-    if (tr[rs(p)].l <= r)   add(rs(p), l, r, val);
-    pushUp(p);
-}
 
 int main() {
     initDebug;
-
-    scanf("%d%d", &N, &M);
-    upto(i, N)  scanf("%lld", init+i);
-
-    build(1, 1, N);
-
-    int OP, x, y, k;
-    upto(_, M) {
-        scanf("%d", &OP);
-        if (OP == 1) {  // 加k
-            scanf("%d%d%d", &x, &y, &k);
-            add(1, x, y, k);
-        } else {  // 查询
-            scanf("%d%d", &x, &y);
-            printf("%lld\n", sum(1, x, y));
-        }
-    }
+    Solution::solve();
     return 0;
 }
-
-#undef rs
-#undef ls
