f1f3702a87fa1fcb6cc411458a2f9e716802f5f1
normal-pcer <normal-pcer@outlook.com>
2024-10-19 17:09:07 +0800

    0

---
+++ test.cpp
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+#include "lib"
+#include "libs/all.hpp"
+using namespace lib;
+
+namespace Solution {
+
+    int N;
+    struct Node {
+        
+    };
+    
+    void init() {
+        io >> N;
+    }
+
+    void solve() {
+        init();
+
+    }
+}
+
+
+int main() {
+    initDebug;
+    Solution::solve();
+    return 0;
+}
