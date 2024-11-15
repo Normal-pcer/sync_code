bf7a33b7ca6079645809c597dcdb5171e6adf53a
normal-pcer <normal-pcer@outlook.com>
2024-11-15 15:11:35 +0800

    0

--- wordleHelper.py
+++ wordleHelper.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Dict, Set
+from typing import List, Tuple, Set
 
 import csv
 import re
@@ -15,11 +15,10 @@ def warn(msg: str):
     print("[警告] {}".format(msg))
 
 
-class Limits:
+class Limit:
     ensured: List[Tuple[int, str]]
     exist: List[Tuple[int, str]]
     without: List[str]
-    count: Dict[str, Tuple[int, int]]
 
     def __init__(self, ensured: List[Tuple[int, str]] | None = None, exist: List[Tuple[int, str]] | None = None, 
                     without: List[str] | None = None):
@@ -33,16 +32,15 @@ class Limits:
             if source[slot] != char:
                 return False
             else:
-                # source = source[:slot] + '.' + source[slot+1:]
-                pass
+                source = source[:slot] + '.' + source[slot+1:]
         
         # 匹配存在性已知的字母
         for slot, char in self.exist:
             for match in re.finditer(char, source):
                 span = match.span()
                 if span != slot:
-                    # pos1, pos2 = span
-                    # source = source[:pos1] + '.' + source[pos2:]  # 去掉这个字母
+                    pos1, pos2 = span
+                    source = source[:pos1] + '.' + source[pos2:]  # 去掉这个字母
                     break
             else:  # 循环被跳出
                 return False
@@ -54,7 +52,7 @@ class Limits:
         return True
 
 if False:
-    lim = Limits(
+    lim = Limit(
         [(0, 'a')],
         [(1, 'p')],
         ['p', 'l', 'e']
@@ -78,51 +76,49 @@ if __name__ == "__main__":
                 if len(row[0]) == length:
                     words.append(Word(row[0], row[1]))
 
-    words = [Word("girl", "")]
+    # words = [Word("girl", "")]
 
     try:
-        lim = Limits()
+        limits: List[Limit] = list()
+        def match(s: str) -> bool:
+            for i in limits:
+                if not i.match(s):
+                    return False
+            return True
         while True:
+            lim = Limit()
             guess = input("猜测的单词: ").lower()
             if guess == '/debug':
-                print(lim.ensured, lim.exist, lim.without)
+                for l in limits:
+                    print(l.ensured, l.exist, l.without)
             elif guess == '/reset':
                 lim.ensured = list()
                 lim.exist = list()
                 lim.without = list()
             elif guess == '/print':
-                solutions = [i for i in words if lim.match(i.text)]
+                solutions = [i for i in words if match(i.text)]
                 if (len(solutions) > 50):
                     if input(f"可能性过多({len(solutions)})，仍要显示？[y/n]").lower() != 'y':
                         continue
                 
                 for sol in solutions:
                     print(sol.text, sol.meaning)
+            elif guess == 'undo':
+                if len(limits):
+                    limits.pop()
+                else:
+                    print("没有未撤销的步骤")
             else:
                 res = [*map(int, input("结果(1=灰, 2=黄, 3=绿): "))]
-                l1 = list()
-                l2 = list()
-                l3 = list()
-                letters: Dict[str, int] = dict()
-                letters_tot: Dict[str, int] = dict()
                 for i in range(len(res)):
                     if res[i] == 1:
-                        l1.append(guess[i])
+                        lim.without.append(guess[i])
                     elif res[i] == 2:
-                        l2.append((i, guess[i]))
-                        letters[guess[i]] += letters.get(guess[i], 0) + 1
+                        lim.exist.append((i, guess[i]))
                     else:
-                        l3.append((i, guess[i]))
-                        letters[guess[i]] += letters.get(guess[i], 0) + 1
-                    letters_tot[guess[i]] += letters_tot.get(guess[i], 0) + 1
-
-                for ch in letters:
-                    ch_min = letters[ch]
-                    ch_max = letters_tot[ch]
-                    
-
-
-
+                        lim.ensured.append((i, guess[i]))
+            
+                limits.append(lim)
     except KeyboardInterrupt:
         exit(0)
     except EOFError:
