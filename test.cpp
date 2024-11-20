c5d5710e31ee1eb49da2e1054b8e64cab61ca064
normal-pcer <normal-pcer@outlook.com>
2024-11-20 08:58:03 +0800

    0

--- test.cpp
+++ test.cpp
@@ -3,28 +3,68 @@
  */
 
 #include "./lib"
-#include "libs/io.hpp"
+
+#include "./libs/io.hpp"
+
 using namespace lib;
 
 
-namespace Solution_1469017748636242 {
-    void solve() {
-        std::vector<std::vector<int>> matrix(4, std::vector<int>(4));
+namespace Solution_3904170373422103 {
+    
+    const int _N = 2e5+5;
+    int N, M;
 
-        for (auto i: std::views::iota(0, 4)) {
-            for (auto j: std::views::iota(0, 4)) {
-                io >> matrix.at(i).at(j);
-            }
+    std::array<int, _N> F;
+    std::array<std::vector<int>, _N> graph;
+
+    auto find(int x) -> int {
+        if (x == F[x])  return x;
+        else  return (F[x] = find(F[x]));
+    }
+
+    auto connect(int x, int y) -> void {
+        auto a = find(x), b = find(y);
+        F[a] = b;
+    }
+    
+    void solve() {
+        io >> N >> M;
+
+        std::vector<std::pair<int, int>> edges;  
+        for (auto _: std::views::iota(0, M)) {
+            int x, y;
+            io >> x >> y;
+            edges.push_back({x, y});
+        }
+        std::vector<int> colors(1);
+        for (auto _: std::views::iota(0, N)) {
+            int color;
+            io >> color;
+            colors.push_back(color);
         }
 
-        
+        for (auto i: std::views::iota(1, N+1)) {
+            F[i] = i;
+        }
+        for (auto [x, y]: edges) {
+            if (colors.at(x) != colors.at(y))  connect(x, y);
+        }
+
+        for (auto [x, y]: edges) {
+            if (colors.at(x) == colors.at(y) and find(x) == find(y)) {
+                io << "Yes" << endl;
+                goto egg;
+            }
+        }
+        io << "No" << endl;
+    egg:
+        ;
     }
 }
 
 
 int main() {
     initDebug;
-    int T;  io >> T;
-    while (T --> 0)  Solution_1469017748636242::solve();
+    Solution_3904170373422103::solve();
     return 0;
 }
--- test.py
+++ test.py
@@ -1,25 +1,14 @@
-def maximize_dissimilarity(n, S):
-    T = []
-    current_char = 'a'
-    for s_char in S:
-        # 选择一个与 s_char 不同的字符
-        # 简单地通过循环 'a' 到 'z' 来实现
-        # 如果 current_char 与 s_char 相同，则跳到下一个字符
-        while current_char == s_char:
-            current_char = chr((ord(current_char) - ord('a') + 1) % 26 + ord('a'))
-        T.append(current_char)
-        # 为了下一次迭代，可能需要更新 current_char
-        # 但由于我们只需要任意一种解，所以这里不需要真正地“跟踪”使用过的字符
-        # 简单地继续循环即可（这会导致在某些情况下字符会重复，但这仍然是一个有效的解）
-        # 如果想要避免在 T 中重复使用相同的字符（尽管这不是必需的），
-        # 可以使用一个集合来跟踪已经使用过的字符，并在选择新字符时进行检查。
-        # 但由于题目要求输出任意一种解，并且 n 可以远大于 26，
-        # 因此这种方法在这里是不必要的。
-    return ''.join(T)
+T = int(input())
 
-# 读取输入
-n = int(input())
-S = input().strip()
-
-# 计算并输出结果
-print(maximize_dissimilarity(n, S))
+for _ in range(T):
+    N, K = map(int, input().split())
+    n = N
+    cnt = 0
+    while n > 0:
+        cnt += n % 3
+        n = n // 3
+    
+    if cnt <= K and (K - cnt) % 2 == 0:
+        print("Yes")
+    else:
+        print("No")
