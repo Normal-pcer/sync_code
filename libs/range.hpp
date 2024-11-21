56400d9e7d5184c8978be02d86dc6a843a2b3c78
normal-pcer <normal-pcer@outlook.com>
2024-11-21 21:02:11 +0800

    0

--- libs/range.hpp
+++ libs/range.hpp
@@ -1,16 +1,18 @@
-template <typename T> auto range(const T to) {  return std::views::iota(T(), to);  }
-template <typename T> auto range(const T begin, const T end) {  return std::views::iota(begin, end);  }
-template <typename T, typename U> struct StepViewIterator {
-    T _cur;  const U _step;
-    StepViewIterator(T cur, const U step): _cur(cur), _step(step) {}
-    auto operator++() { _cur+=_step; }  auto operator++(int) { _cur+=_step; }
-    auto operator!=(const StepViewIterator& other) { return _cur<other._cur; }
-    auto operator*() { return _cur; }
-};
-template <typename T, typename U> struct StepView: std::ranges::view_interface<StepView<T, U>> {
-    const T _begin, _end;  const U _step;
-    StepView(const T begin, const T end, const U step): _begin(begin), _end(end), _step(step) {}
-    auto begin() const { return StepViewIterator<T, U>(_begin, _step); }
-    auto end() const { return StepViewIterator<T, U>(_end, _step); }
-};
-template <typename T, typename U> auto range(T begin, T end, U step) { return StepView<T, U>(begin, end, step); }
+namespace lib {   
+    template <typename T> auto range(const T to) {  return std::views::iota(T(), to);  }
+    template <typename T> auto range(const T begin, const T end) {  return std::views::iota(begin, end);  }
+    template <typename T, typename U> struct StepViewIterator {
+        T _cur;  const U _step;
+        StepViewIterator(T cur, const U step): _cur(cur), _step(step) {}
+        auto operator++() { _cur+=_step; }  auto operator++(int) { _cur+=_step; }
+        auto operator!=(const StepViewIterator& other) { return _cur<other._cur; }
+        auto operator*() { return _cur; }
+    };
+    template <typename T, typename U> struct StepView: std::ranges::view_interface<StepView<T, U>> {
+        const T _begin, _end;  const U _step;
+        StepView(const T begin, const T end, const U step): _begin(begin), _end(end), _step(step) {}
+        auto begin() const { return StepViewIterator<T, U>(_begin, _step); }
+        auto end() const { return StepViewIterator<T, U>(_end, _step); }
+    };
+    template <typename T, typename U> auto range(T begin, T end, U step) { return StepView<T, U>(begin, end, step); }
+}
--- test.cpp
+++ test.cpp
@@ -4,31 +4,86 @@
 
 #include "./lib"
 
-#include "./libs/range.hpp"
+#include "./libs/io.hpp"
 
 using namespace lib;
 
-namespace Solution_4153386203017484 {
+namespace Solution_1957976573789557 {
+    
+    int N;  const int _N = 32768;
+    std::vector<int> values {-inf};
+    int c[_N];
 
-    std::mt19937 rnd(745184);
+    inline auto getIndex(int x) {
+        return std::lower_bound(values.begin(), values.end(), x) - values.begin();
+    }
+
+    inline auto lowbit(int x) { return x & -x; }
+
+    int query(int x) {
+        auto res = 0;
+        while (x) {
+            res += c[x];
+            x -= lowbit(x);
+        }
+        return res;
+    }
+
+    void update(int x, int val) {
+        while (x < _N) {
+            c[x] += val;
+            x += lowbit(x);
+        }
+    }
 
     void solve() {
-        ull res = 0ULL;
-#if true
-        for (auto _: range(1LL, 1'000'000'001LL)) {
-            res ^= rnd();
+        io >> N;
+        std::vector<int> a(N);
+        from(i, 0, N)  io >> a[i];
+
+        values.resize(a.size()+1);
+        std::copy(a.begin(), a.end(), values.begin()+1);  // values 中保留一个值，避免树状数组插入第 0 位
+        std::sort(values.begin()+1, values.end());
+
+        std::vector<int> less(N);  // i 左侧更小的值数量
+        std::vector<int> greater(N);  // 右侧更大
+
+        from(i, 0ULL, a.size()) {
+            auto cur = a[i];
+            less[i] = query(getIndex(cur) - 1);
+            update(getIndex(cur), 1);
         }
-#else
-        for (auto _ = 1LL; _ < 1'000'000'001LL; _++) {
-            res ^= rnd();
+
+        std::memset(c, 0, sizeof(c));
+
+        for (auto i = a.size(); i > 0; i--) {
+            auto tot = a.size() - i;  // 经历过的元素
+            auto cur = a[i-1];
+            auto this_leq = query(getIndex(cur));
+            auto this_greater = tot - this_leq;
+            greater[i-1] = this_greater;
+            update(getIndex(cur), 1);
         }
-#endif
-        std::cout << res << std::endl;
+
+        never {
+            for (auto i: less)  io << i << ' ';
+            io << endl;
+            for (auto i: greater)  io << i << ' ';
+            io << endl;
+        }
+
+        auto ans = 0LL;
+        from(i, 0ULL, a.size()) {
+            auto cur = (ll)less[i] * greater[i];
+            ans += cur;
+        }
+
+        io << ans << endl;
     }
 }
 
 int main() {
     initDebug;
-    Solution_4153386203017484::solve();
+    Solution_1957976573789557::solve();
     return 0;
 }
