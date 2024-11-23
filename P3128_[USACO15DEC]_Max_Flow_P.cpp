d272cac7499391b0d0f1a380f241f3058d16976d
normal-pcer <normal-pcer@outlook.com>
2024-11-23 17:12:48 +0800

    0

---
+++ P3128_[USACO15DEC]_Max_Flow_P.cpp
@@ -0,0 +1,88 @@
+/**
+ * @link https://www.luogu.com.cn/problem/P3128
+ */
+
+#include "./lib"
+
+#include "./libs/range.hpp"
+
+using namespace lib;
+
+namespace Solution_7216935459152744 {
+
+    int N, K;
+    const int _N = 5e4+5, _ST = 20;
+    int ST;
+    std::vector<std::vector<int>> graph;
+
+    std::vector<int> diff;  // 点权的差分
+    std::vector<int> depth;
+    std::vector<std::vector<int>> F;  // 倍增
+
+    void dfs_pre(int p, int prev) {
+        depth.at(p) = depth.at(prev) + 1;
+        F.at(p).at(0) = prev;
+        for (auto i: range(1ULL, F.at(p).size())) {
+            F.at(p).at(i) = F.at( F.at(p).at(i-1) ).at(i-1);
+        }
+        for (auto dest: graph.at(p))  if (dest != prev) {
+            dfs_pre(dest, p);
+        }
+    }
+
+    std::pair<int, bool> LCA(int x, int y) {
+        auto distance = depth.at(x) - depth.at(y);
+        for (auto i = 0; i < ST; i++) {
+            if (distance & (1<<i)) {
+                x = F.at(x).at(i);
+            }
+        }
+        if (x == y)  return {x, true};
+        for (int i = ST-1; i >= 0; i--) {
+            if (F.at(x).at(i) != F.at(y).at(i)) {
+                x = F.at(x).at(i), y = F.at(y).at(i);
+            }
+        }
+
+        return {F.at(x).at(0), false};
+    }
+
+    void modify(int x, int y) {
+        if (depth.at(x) < depth.at(y))  std::swap(x, y);
+        auto [ancestor, single] = LCA(x, y);
+        diff.at(F.at(ancestor).at(0))--;
+        diff.at(x)++;
+
+        if (not single) {
+            diff.at(y)++;
+            diff.at(F.at(ancestor).at(0))--;
+        }
+    }
+
+    void solve() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+        std::cin >> N >> K;
+        graph.resize(N+1);
+        for (auto _: range(N-1)) {
+            int x, y;  std::cin >> x >> y;
+            graph.at(x).push_back(y), graph.at(y).push_back(x);
+        }
+
+        ST = std::__lg(N) + 1;
+        depth.resize(N+1), std::fill_n(std::back_inserter(F), N+1, std::vector<int>(std::__lg(ST)));
+        for (auto _: range(K)) {
+            int x, y;  std::cin >> x >> y;
+            modify(x, y);
+        }
+
+        
+    }
+}
+
+int main() {
+    initDebug;
+    Solution_7216935459152744::solve();
+    return 0;
+}
