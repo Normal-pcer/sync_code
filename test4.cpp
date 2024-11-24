bc1b0de9fc5ac13ad555687f92d451b6d64d585b
normal-pcer <normal-pcer@outlook.com>
2024-11-24 21:40:40 +0800

    0

--- test4.cpp
+++ test4.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 
 int main() {
+    std::set<int> s {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    if (s.contains(5))  std::cout << "Yes" << std::endl;
+    else  std::cout << "No" << std::endl;
     return 0;
 }
