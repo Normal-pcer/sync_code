6401883485beca2e1b4af49f22e9452294019e30
normal-pcer <normal-pcer@outlook.com>
2024-11-24 21:47:42 +0800

    0

--- test4.cpp
+++ test4.cpp
@@ -1,8 +1,12 @@
 #include <bits/stdc++.h>
 
-int main() {
+auto main() -> int {
     std::set<int> s {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    if (s.contains(5))  std::cout << "Yes" << std::endl;
-    else  std::cout << "No" << std::endl;
+    constexpr const int x = 5;
+    auto check = [&s](int x) -> void {
+        if (s.contains(x))  std::print("Yes, there is {}!", x);
+        else  std::print("No, this is not {}!", x);
+    };
+    check(x);
     return 0;
 }
