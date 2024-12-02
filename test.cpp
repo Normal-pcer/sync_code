551ed0aa5f27cddc5bd29b00a7b790f58c80c8cc
normal-pcer <normal-pcer@outlook.com>
2024-12-03 07:42:16 +0800

    0

---
+++ test.cpp
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+#include "lib"
+#include "libs/io.hpp"
+using namespace lib;
+
+namespace Solution_1386135091856135 {
+
+    int N;
+    struct Op {
+        // int 
+    };
+    // std::vector<>
+
+    void solve() {
+        io >> N;
+    }
+}
+
+int main() {
+    initDebug;
+    Solution_1386135091856135::solve();
+    return 0;
+}
