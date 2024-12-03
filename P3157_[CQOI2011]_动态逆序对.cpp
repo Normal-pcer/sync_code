3a6cb0700035ae26d65e390c7fe36ffef0fd5c35
normal-pcer <normal-pcer@outlook.com>
2024-12-03 21:03:09 +0800

    0

---
+++ P3157_[CQOI2011]_动态逆序对.cpp
@@ -0,0 +1,151 @@
+/**
+ * @link https://www.luogu.com.cn/problem/P3157
+ */
+
+#include "./lib"
+
+#include "./libs/io.hpp"
+
+#include "./libs/range.hpp"
+
+using namespace lib;
+
+namespace Solution_1471250357606197 {
+    class BIT {
+        std::vector<ll> c;
+        std::vector<std::pair<int, int>> log;
+
+        inline constexpr int lowbit(int x) const noexcept { return x & -x; };
+    public:
+        BIT(size_t N = 0): c(N+2) {}
+        ll query(int x) const {
+            x++;  auto res = 0LL;
+            while (x) {
+                res += c.at(x);
+                x -= lowbit(x);
+            }
+            return res;
+        }
+
+        void update(int x, int val) {
+            log.emplace_back(x, val), x++;
+            while (x < (int)c.size()) {
+                c.at(x) += val;
+                x += lowbit(x);
+            }
+        }
+
+        void clear() {
+            size_t size = log.size();
+            for (auto i = 0; i != (int)size; i++) {
+                auto [x, val] = log.at(i);
+                update(x, -val);
+            }
+            log.clear();
+        }
+    } tree;
+
+    struct Element {
+        int i;  // 原始下标
+        int x;  // 原始值
+        int t;  // 删除次序
+    };
+
+    int N, M;
+    std::vector<Element> ele;
+    std::vector<int> pos;
+    std::vector<int> expire;
+
+    std::vector<ll> ans1;
+    void cdq1(int begin, int end) {
+        if (begin + 1 == end)  return;
+
+        auto mid = (begin + end) >> 1;
+        cdq1(begin, mid), cdq1(mid, end);
+
+        auto i = begin, j = mid, k = 0;
+        std::vector<Element> tmp(end - begin);
+        tree.clear();
+        while (i != mid or j != end) {
+            if (j == end or (i != mid and ele.at(i).i <= ele.at(j).i)) {
+                tree.update(ele.at(i).t, 1);
+                tmp.at(k++) = ele.at(i++);
+            } else {
+                ans1.at(ele.at(j).i) += tree.query(ele.at(j).t);
+                tmp.at(k++) = ele.at(j++);
+            }
+        }
+        std::copy(tmp.begin(), tmp.end(), ele.begin() + begin);
+    }
+
+    std::vector<ll> ans2;
+    void cdq2(int begin, int end) {
+        if (begin + 1 == end)  return;
+
+        auto mid = (begin + end) >> 1;
+        cdq2(begin, mid), cdq2(mid, end);
+
+        auto i = begin, j = mid, k = 0;
+        std::vector<Element> tmp(end - begin);
+        tree.clear();
+        while (i != mid or j != end) {
+            if (j == end or (i != mid and ele.at(i).i >= ele.at(j).i)) {
+                tree.update(ele.at(i).t, 1);
+                tmp.at(k++) = ele.at(i++);
+            } else {
+                ans2.at(ele.at(j).i) += tree.query(ele.at(j).t);
+                tmp.at(k++) = ele.at(j++);
+            }
+        }
+        std::copy(tmp.begin(), tmp.end(), ele.begin() + begin);
+    }
+
+    void solve() {
+        io >> N >> M;
+        ele.resize(N), pos.resize(N+1);
+        for (auto i = 0; i != (int)ele.size(); i++) {
+            ele[i].i = i;
+            io >> ele[i].x;
+            pos.at(ele[i].x) = i;
+        }
+
+        expire.resize(M);
+        for (auto i = 0; i != (int)expire.size(); i++)  io >> expire[i], ele.at(pos.at(expire[i])).t = M - i;
+
+        debug for (auto [i, x, t]: ele)  io << i << ' ' << x << ' ' << t << endl;
+
+        tree = BIT(N);
+
+        ans1.resize(N);
+        std::sort(ele.begin(), ele.end(), lam(x, y, x.x > y.x));  // t_A < t_B
+        cdq1(0, N);
+
+        ans2.resize(N);
+        std::sort(ele.begin(), ele.end(), lam(x, y, x.x < y.x));  // t_A > t_B
+        cdq2(0, N);
+
+        std::vector<ll> ans_total(N);
+        std::transform(ans1.begin(), ans1.end(), ans2.begin(), ans_total.begin(), lam(x, y, x+y));
+
+        debug {
+            for (auto i: ans1)  io << i << ' ';
+            io << endl;
+            for (auto i: ans2)  io << i << ' ';
+            io << endl;
+            for (auto i: ans_total)  io << i << ' ';
+            io << endl;
+        }
+
+        auto sum = std::accumulate(ans_total.begin(), ans_total.end(), 0LL);
+        for (auto i = 0; i != M; i++) {
+            io << sum << endl;
+            sum -= ans_total.at(pos.at(expire.at(i)));
+        }
+    }
+}
+
+int main() {
+    initDebug;
+    Solution_1471250357606197::solve();
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,68 +1,16 @@
-/**
- * @link http://localhost/
- */
-
 #include "./lib"
 
-#include "./libs/io.hpp"
-
-using namespace lib;
-
-namespace Solution_5736322220169565 {
-
-    struct Node {
-        int pos, val;
-
-        bool operator< (const Node &other) const noexcept {
-            if (pos == other.pos)  return val < other.val;
-            else  return pos < other.pos;
-        }
-    };
-
-    std::vector<Node> a;
-    std::vector<int> F;
-
-    void cdq(int begin, int end) {
-        if (begin + 1 == end)  return void(chkMax(F.at(begin), 1));
-        auto mid = (begin + end) >> 1;
-        cdq(begin, mid);
-        
-        auto with_val = lam(x, y, x.val < y.val);
-        std::sort(a.begin() + begin, a.begin() + mid, with_val);
-        std::sort(a.begin() + mid, a.begin() + end, with_val);
-
-        auto i = begin, j = mid;
-        
-        auto max = 0;  // 左侧 F 的最大值
-        while (j != end) {
-            while (i != mid and a.at(i).val <= a.at(j).val)  chkMax(max, F.at(a.at(i++).pos));
-            if (i != begin)  i--;
-            chkMax(F.at(a.at(j).pos), max + 1);
-            j++;
-        }
-        std::sort(a.begin() + mid, a.begin() + end);
-        cdq(mid, end);
-    }
-
-    void solve() {
-        int N;  io >> N;
-        N++;  a.resize(N), F.resize(N);
-
-        for (auto i = 0; i < N - 1; i++) {
-            a.at(i).pos = i;
-            io >> a.at(i).val;
-        }
-
-        a.back() = {N-1, inf};
-
-        cdq(0, N);
-
-        io << F.at(N-1) - 1 << endl;
-    }
-}
-
 int main() {
-    initDebug;
-    Solution_5736322220169565::solve();
+    std::vector<int> v1 {0, 1, 2};
+    std::vector<int> v2 {3, 4, 5};
+
+                        
+
+    std::vector<int> res;
+    res.resize(v1.size());
+    std::transform(v1.begin(), v1.end(), v2.begin(), res.begin(), lam(x, y, x*y));
+
+    for (auto i: res)  std::cout << i << ' ';
+    std::cout << std::endl;
     return 0;
 }
