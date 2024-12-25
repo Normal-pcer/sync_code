07ad9769040dceac3bffca11f715f6d4923a750b
normal-pcer <normal-pcer@outlook.com>
2024-12-25 08:21:19 +0800

    0

--- test.cpp
+++ test.cpp
@@ -1,51 +1,91 @@
 #include <bits/stdc++.h>
-#include "./libs/debug_log.hpp"
 
-int N;
-struct Node {
-    int begin, end;
-    bool flag = false;
+class SegTree {
+    struct Node {
+        int begin, end;
+        long long sum = 0, add_tag = 0;
+    };
+    std::vector<Node> tr;
+    std::vector<long long> &init;
+    int len(int p) const { return tr[p].end - tr[p].begin; }
+    int mid_(int p) const { return std::midpoint(tr[p].begin, tr[p].end); }
+#define ls (mid<<1)
+#define rs (mid<<1 | 1)
+    void pushUp(int p) {
+        auto mid = mid_(p);
+        tr[p].sum = tr[ls].sum + tr[rs].sum;
+    }
+    void addNode(int p, int val) {
+        tr[p].sum += val * len(p);
+        tr[p].add_tag += val;
+    }
+    void pushDown(int p) {
+        if (tr[p].add_tag) {
+            auto mid = mid_(p);
+            for (auto s: {ls, rs}) {
+                addNode(s, tr[p].add_tag);
+            }
+            tr[p].add_tag = 0;
+        }
+    }
+    void build(int begin, int end, int p = 1) {
+        tr[p].begin = begin, tr[p].end = end;
+        if (begin + 1 == end) {
+            tr[p].sum = init.at(begin);
+            return;
+        }
+        auto mid = mid_(p);
+        build(begin, mid, ls), build(mid, end, rs);
+        pushUp(p);
+    }
+public:
+    SegTree(int N, std::vector<long long> &init): tr(N << 1), init(init) {
+        build(0, N);
+    }
+    long long sum(int begin, int end, int p = 1) {
+        if (tr[p].begin >= begin and tr[p].end <= end) {
+            return tr[p].sum;
+        }
+        pushDown(p);
+        auto res = 0LL;
+        auto mid = mid_(p);
+        if (tr[ls].end > begin)  res += sum(begin, end, ls);
+        if (tr[rs].begin < end)  res += sum(begin, end, rs);
+        return res;
+    }
+    void add(int begin, int end, long long val, int p = 1) {
+        if (tr[p].begin >= begin and tr[p].end <= end) {
+            addNode(p, val);
+            return;
+        }
+        pushDown(p);
+        auto mid = mid_(p);
+        if (tr[ls].end > begin)  add(begin, end, val, ls);
+        if (tr[rs].begin < end)  add(begin, end, val, rs);
+        pushUp(p);
+    }
+#undef ls
+#undef rs
 };
-Node tr[10000];
-std::set<int> set;
-int ans = 0;
-void build(int begin, int end, int p = 1) {
-    std::cout << begin << ' ' << end << std::endl;
-    ans++;
-    tr[p].begin = begin, tr[p].end = end, tr[p].flag = true;
-    auto x = (((begin + end) | 1)^ (begin + 1 == end));
-    std::cout << x << std::endl;
-    set.insert(x);
-    if (begin + 1 == end)  return;
-    auto mid = std::midpoint(begin, end);
-    build(begin, mid, p << 1), build(mid, end, p << 1 | 1);
-}
-
-void build2(int l, int r, int p = 1) {
-    ans++;
-    tr[p].begin = l, tr[p].end = r;
-    auto x = ((l + r) | (l != r)) - 1;
-    set.insert(x);
-    if (l == r)  return;
-    auto mid = std::midpoint(l, r);
-    build2(l, mid), build2(mid+1, r);
-}
-
-void build3(int begin, int end, int p = 1) {
-    std::cout << begin << ' ' << end << ' ' << p << std::endl;
-    ans++;
-    tr[p].begin = begin, tr[p].end = end, tr[p].flag = true;
-    std::cout << p << std::endl;
-    set.insert(p);
-    if (begin + 1 == end)  return;
-    auto mid = std::midpoint(begin, end);
-    build(begin, mid, mid << 1), build(mid, end, mid << 1 | 1);
-}
 
 int main() {
-    int begin, end;
-    std::cin >> begin >> end;
-    build3(begin, end, std::midpoint(begin, end));
-    printValues(ans, set.size());
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+    int N, M;  std::cin >> N >> M;
+    std::vector<long long> init(N+1);
+    for (auto &i: init | std::views::drop(1))  std::cin >> i; 
+    SegTree sgt(N+1, init);
+    for (auto _ = 0; _ != M; _++) {
+        int op, x, y;
+        std::cin >> op >> x >> y;
+        if (op == 1) {
+            int k;  std::cin >> k;
+            sgt.add(x, y+1, k);
+        } else {
+            auto ans = sgt.sum(x, y+1);
+            std::cout << ans << '\n';
+        }
+    }
     return 0;
-} 
+}
---
+++ test2.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+class SegTree {
+    struct Node {
+        int begin, end;
+        long long sum = 0, add_tag = 0;
+    };
+    std::vector<Node> tr;
+    std::vector<long long> &init;
+    int len(int p) const { return tr[p].end - tr[p].begin; }
+    int mid(int p) const { return std::midpoint(tr[p].begin, tr[p].end); }
+#define ls (p<<1)
+#define rs (p<<1 | 1)
+    void pushUp(int p) {
+        tr[p].sum = tr[ls].sum + tr[rs].sum;
+    }
+    void addNode(int p, int val) {
+        tr[p].sum += val * len(p);
+        tr[p].add_tag += val;
+    }
+    void pushDown(int p) {
+        if (tr[p].add_tag) {
+            for (auto s: {ls, rs}) {
+                addNode(s, tr[p].add_tag);
+            }
+            tr[p].add_tag = 0;
+        }
+    }
+    void build(int begin, int end, int p = 1) {
+        tr[p].begin = begin, tr[p].end = end;
+        if (begin + 1 == end) {
+            tr[p].sum = init.at(begin);
+            return;
+        }
+        build(begin, mid(p), ls), build(mid(p), end, rs);
+        pushUp(p);
+    }
+public:
+    SegTree(int N, std::vector<long long> &init): tr(N << 2), init(init) {
+        build(0, N);
+    }
+    long long sum(int begin, int end, int p = 1) {
+        if (tr[p].begin >= begin and tr[p].end <= end) {
+            return tr[p].sum;
+        }
+        pushDown(p);
+        auto res = 0LL;
+        if (tr[ls].end > begin)  res += sum(begin, end, ls);
+        if (tr[rs].begin < end)  res += sum(begin, end, rs);
+        return res;
+    }
+    void add(int begin, int end, long long val, int p = 1) {
+        if (tr[p].begin >= begin and tr[p].end <= end) {
+            addNode(p, val);
+            return;
+        }
+        pushDown(p);
+        if (tr[ls].end > begin)  add(begin, end, val, ls);
+        if (tr[rs].begin < end)  add(begin, end, val, rs);
+        pushUp(p);
+    }
+#undef ls
+#undef rs
+};
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+    int N, M;  std::cin >> N >> M;
+    std::vector<long long> init(N+1);
+    for (auto &i: init | std::views::drop(1))  std::cin >> i; 
+    SegTree sgt(N+1, init);
+    for (auto _ = 0; _ != M; _++) {
+        int op, x, y;
+        std::cin >> op >> x >> y;
+        if (op == 1) {
+            int k;  std::cin >> k;
+            sgt.add(x, y+1, k);
+        } else {
+            auto ans = sgt.sum(x, y+1);
+            std::cout << ans << '\n';
+        }
+    }
+    return 0;
+}
