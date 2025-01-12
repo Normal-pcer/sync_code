12e95389c764e9625d46295dba26d00fdff419ec
normal-pcer <normal-pcer@outlook.com>
2025-01-13 01:09:23 +0800

    0

---
+++ P3401_洛谷树.cpp
@@ -0,0 +1,18 @@
+/**
+ * @link https://www.luogu.com.cn/problem/P3401
+ */
+
+#include "lib"
+using namespace lib;
+
+namespace Solution_1983728601745017 {
+    void solve() {
+
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    DEBUG_MODE = (argc-1) and not strcmp("-d", argv[1]);
+    Solution_1983728601745017::solve();
+    return 0;
+}
