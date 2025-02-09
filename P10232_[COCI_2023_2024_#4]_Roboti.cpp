b778dc3462915f51a89b52ef9be104add4d030b8
normal-pcer <normal-pcer@outlook.com>
2025-02-09 22:39:43 +0800

    0

---
+++ P10232_[COCI_2023_2024_#4]_Roboti.cpp
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+#include "lib"
+using namespace lib;
+
+namespace Solution_8670256859201745 {
+    void solve() {
+        
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    DEBUG_MODE = (argc-1) and not strcmp("-d", argv[1]);
+    Solution_8670256859201745::solve();
+    return 0;
+}
