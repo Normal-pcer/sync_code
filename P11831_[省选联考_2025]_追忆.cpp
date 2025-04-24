caf222b6694891ba17897d817e42c33cbb7c7458
normal-pcer <normal-pcer@outlook.com>
2025-04-24 17:25:28 +0800

    0

--- P11831_[省选联考_2025]_追忆.cpp
+++ P11831_[省选联考_2025]_追忆.cpp
@@ -1,9 +1,35 @@
 /**
  * @link https://www.luogu.com.cn/problem/P11831
  */
-#include "./lib_v7.hpp"
-#include "./libs/fixed_int.hpp"
+#include <bits/stdc++.h>
+bool DEBUG_MODE = false;
+#ifdef LOCAL
+#define debug if (DEBUG_MODE)
+#else
+#define debug if constexpr (false)
+#endif
+#define never if constexpr (false)
+template <typename T> inline auto chkMax(T &base, const T &cmp) -> T & { return (base = std::max(base, cmp)); }
+template <typename T> inline auto chkMin(T &base, const T &cmp) -> T & { return (base = std::min(base, cmp)); }
+#define _lambda_1(expr) [&]() { return expr; }
+#define _lambda_2(a, expr) [&](auto a) { return expr; }
+#define _lambda_3(a, b, expr) [&](auto a, auto b) { return expr; }
+#define _lambda_4(a, b, c, expr) [&](auto a, auto b, auto c) { return expr; }
+#define _lambda_overload(a, b, c, d, e, ...) _lambda_##e
+#define lambda(...) _lambda_overload(__VA_ARGS__, 4, 3, 2, 1)(__VA_ARGS__)
+#define lam lambda
+namespace lib {
+#if __cplusplus > 201703LL
+    namespace ranges = std::ranges;
+    namespace views = std::views;
+#endif
+}
+char constexpr endl = '\n';
 using namespace lib;
+#include "./libs/fixed_int.hpp"
+
+using namespace lib;
+namespace chrono = std::chrono;
 
 /*
 「我该在哪里停留？我问我自己。」
@@ -29,17 +55,21 @@ limit[v] in [bsize_lim * i, bsize_lim * (i + 1))
 这样就可以把所有合法的整块，一起加入一个点集中。但是复杂度是 sqrt(n) * sqrt(n)。我们一般称为 O(n)。
 不过注意到查询很多次，并且修改相对简单很多（每个块上的修改是 O(1) 的）。于是使用前缀和优化。
 
+（修正：为了和后面的统一，这里也改用后缀和）
 用块 b_lim[i] 来维护一个点集，使得点 v 满足：
-limit[v] in [0, bsize_lim * (i + 1))
+// limit[v] in [0, bsize_lim * (i + 1))
+limit[v] in [bsize_lim * i, n + 1)
 
 这样所有合法的整块，可以转化为两个点集“相减”。散块直接暴力加入这个点集。
 这个步骤的复杂度：
-n / w + n / bsize_lim
+n / w + bsize_lim
 
 通过以上方法维护出来了点集 A，然后考虑找到最大的 value。
 对于 values 的值域分块处理。记块长为 bsize_val。
+（修正：这里也采用后缀和；前缀和只能用来找到最小值）
 用块 b_val[i] 来维护一个点集，使得每个点 v 满足：
-value[v] in [0, bsize_val * (i + 1))
+// value[v] in [0, bsize_val * (i + 1))
+value[v] in [bsize_lim * i, n + 1)
 
 接下来，可以发现，A 和 b_val[i].set 是否存在交集，是存在单调性的。
 于是可以使用二分查找最大的出现合法点的值域块。接下来选择块内 value[v] 最大的即可。
@@ -54,10 +84,87 @@ n * log(n / bsize_val) / w + bsize_val
 修改可能会改变两个点的 limit 和 value。
 所有的 limits 块或者 values 块都会被单点修改。
 复杂度为：(n / bsize_lim) + (n / bsize_val)
+
+但是这个被卡脖子了，跑不过暴力。
+考虑查询时做如下的优化：
+每次拿可行点集的一位，与每个 values 分块进行匹配。如果匹配不到，就切换为下一个位继续匹配。类似双指针。
+由于后缀和对每一位都有单调性，所以这个方法显然是正确的。
+每一次这一位不匹配，接下来的所有块，一定也不包含这一位了。
+最后可以得到第一个完全不匹配的位置。
+实际上不用每次取一位，每次取 64 位即可做到 n/w。
+
+n * log(n / bsize_val) / w + bsize_val
+-> n / w + n / bsize_val + bsize_val
 */
 namespace Solution_1227247956830917 {
+    // 手写 bitset
+    template <uz size>
+    class Bitset {
+    public:
+        using BlockType = u64;
+        std::array<BlockType, ((size + 63) >> 6)> data;
+
+        Bitset(): data() {}  // 初始化为全 0
+
+        auto static constexpr maskAt(uz index) -> BlockType { return (BlockType)1 << (index & 63); }
+        auto flip(uz index) -> void { data[index >> 6] ^= maskAt(index & 63); }  // 反转
+        auto test(uz index) const -> bool { return data[index >> 6] & maskAt(index & 63); }  // 读取
+        auto unset(uz index) -> void { data[index >> 6] &= ~maskAt(index & 63); }  // 设为 0
+        auto set(uz index) -> void { data[index >> 6] |= maskAt(index & 63); }  // 设为 1
+        auto assign(uz index, bool to) -> void { to? set(index): unset(index); }  // 设置
+        auto operator|= (Bitset const &other) -> Bitset & {
+            for (uz i = 0; i < data.size(); i++) data[i] |= other.data[i];
+            return *this;
+        }
+        auto operator^= (Bitset const &other) -> Bitset & {
+            for (uz i = 0; i < data.size(); i++) data[i] ^= other.data[i];
+            return *this;
+        }
+        auto operator&= (Bitset const &other) -> Bitset & {
+            for (uz i = 0; i < data.size(); i++) data[i] &= other.data[i];
+            return *this;
+        }
+        auto operator| (Bitset const &other) const -> Bitset {
+            Bitset res = *this;
+            return res |= other;
+        }
+        auto operator& (Bitset const &other) const -> Bitset {
+            Bitset res = *this;
+            return res &= other;
+        }
+        auto operator^ (Bitset const &other) const -> Bitset {
+            Bitset res = *this;
+            return res ^= other;
+        }
+        auto operator~ () const -> Bitset {
+            Bitset res = *this;
+            for (uz i = 0; i < data.size(); i++) data[i] = ~data[i];
+            return res;
+        }
+        auto operator[] (uz index) const -> bool {
+            return test(index);
+        }
+        auto friend operator<< (std::ostream &os, Bitset const &bits) -> std::ostream & {
+            for (uz i = size; i --> 0; ) os << bits.test(i);
+            return os;
+        }
+        auto none() const -> bool {
+            return ranges::none_of(data, lam(x, x != 0));
+        }
+    };
+
     i32 constexpr maxN = 100'008;
-    using PointsSet = std::bitset<maxN>;
+    using PointSet = Bitset<maxN>;
+
+    template <typename T, typename U, typename Func, typename Comp = std::less<>>
+    auto lowerBoundValue(T begin, T end, U x, Func &&func, Comp &&cmp = {}) -> T {
+        while (end - begin >= 1) {
+            auto mid = std::midpoint(begin, end);
+            if (cmp(func(mid), x)) begin = mid + 1;
+            else end = mid;
+        }
+        return begin;
+    }
 
     class Graph {
         i32 n;
@@ -92,14 +199,15 @@ namespace Solution_1227247956830917 {
     public:
         Graph(i32 n, std::vector<std::pair<i32, i32>> const &edges): n(n), graph(n + 1), outDeg(n + 1) {
             for (auto [u, v]: edges) outDeg[u]++;
+            for (i32 u = 1; u <= n; u++) graph[u].reserve(outDeg[u]);
             for (auto [u, v]: edges) graph[u].push_back(v);
         }
 
         // 预处理一个点能到达的点集
-        auto getReachable() const -> std::vector<PointsSet> {
-            std::vector<PointsSet> reach(n + 1);
+        auto getReachable() const -> std::vector<PointSet> {
+            std::vector<PointSet> reach(n + 1);
             for (i32 u = 1; u <= n; u++) {
-                reach[u][u] = true;
+                reach[u].set(u);
             }
 
             auto order = topoSort();
@@ -110,6 +218,37 @@ namespace Solution_1227247956830917 {
             }
 
             return reach;
+        }
+    };
+
+    // 计时器，用于分析运行速度
+    class Timer {
+        using Clock = chrono::system_clock;
+
+        std::map<std::string, Clock::duration> static inline time;
+        std::string label;
+        Clock::time_point prevTime;
+        bool enabled = true;
+    public:
+        Timer(std::string label): label(std::move(label)), prevTime(Clock::now()) {}
+        ~Timer() { stop(); }
+
+        auto stop() -> void {
+            if (not enabled) return;
+            time[label] += Clock::now() - prevTime;
+            enabled = false;
+        }
+
+        auto start() -> void {
+            prevTime = Clock::now();
+            enabled = true;
+        }
+
+        auto static getTable() -> std::map<std::string, Clock::duration> const & {
+            return time;
+        }
+        auto static clearTable() -> void {
+            time.clear();
         }
     };
 
@@ -156,38 +295,37 @@ namespace Solution_1227247956830917 {
 #else
     class Blocks {
         struct Block {
-            PointsSet data;
+            PointSet owned;
         };
         std::vector<Block> blocks;
 
         i32 n;
-        i32 blockSize;  // 第 i 个块维护 [0, blockSize * i) 值域区间的信息
-        i32 blockCount;
+        i32 blockSize;  // 第 i 个块维护 [blockSize * i, n + 1) 值域区间的信息
+        i32 blockCount;  // 现在有多少个块
 
         std::vector<i32> valueOf;
         std::vector<i32> pointOf;
     public:
-        Blocks(i32 n): n(n), blockSize(static_cast<i32>(1)), valueOf(n + 1), pointOf(n + 1) {
-            blockCount = n / blockSize + 2;
+        Blocks(i32 n, i32 blockSize): n(n), blockSize(blockSize), valueOf(n + 1), pointOf(n + 1) {
+            blockCount = getBlockOf(n) + 1;
             blocks.resize(blockCount);
         }
 
-        auto constexpr blockBegin(i32) const -> i32 {
-            return 0;
-        }
-        auto constexpr blockEnd(i32 i) const -> i32 {
+        auto constexpr blockBegin(i32 i) const -> i32 {
             return blockSize * i;
         }
+        auto constexpr blockEnd(i32) const -> i32 {
+            return n + 1;
+        }
         auto constexpr getBlockOf(i32 value) const -> i32 {
-            return value / blockSize + 1;
+            return value / blockSize;
         }
 
         auto insert(i32 point, i32 value) -> void {
             assert(valueOf[point] == 0);  // 没有被插入过
-            for (i32 i = 0; i < blockCount; i++) {
-                if (value < blockEnd(i)) continue;
-                // 如果这个点位于块的管理值域，则记录
-                blocks[i].data[point] = true;
+            for (i32 i = 0; value >= blockBegin(i); i++) {
+                assert(not blocks[i].owned[point]);
+                blocks[i].owned.set(point);
             }
             valueOf[point] = value;
             pointOf[value] = point;
@@ -195,68 +333,120 @@ namespace Solution_1227247956830917 {
         auto erase(i32 point) -> void {
             assert(valueOf[point] != 0);  // 已经存在
             auto value = valueOf[point];
-            for (i32 i = 0; value < blockSize * i; i++) {
-                blocks[i].data[point] = false;
+            for (i32 i = 0; value >= blockBegin(i); i++) {
+                assert(blocks[i].owned[point]);
+                blocks[i].owned.unset(point);
             }
             valueOf[point] = 0;
             pointOf[value] = 0;
         }
 
         // 获取位于一个值域内的子点集
-        auto getPointsBetween(i32 min, i32 max, PointsSet const &super) const -> PointsSet {
+        auto getPointsBetween(i32 min, i32 max, PointSet const &super) const -> PointSet {
             debug {
                 std::cerr << "blocks: " << std::endl;
                 for (auto const &block: blocks) {
-                    std::cerr << block.data << std::endl;
+                    std::cerr << block.owned << std::endl;
                 }
             }
-            auto block = getBlockOf(max);
-            auto extraBlock = getBlockOf(min);
 
-            PointsSet res{};
+            // 第一个整块和最后一个整块的下一个
+            // total ^ extra 即为中间所有整块的贡献
+            auto totalBlock = getBlockOf(min) + 1;
+            auto extraBlock = getBlockOf(max);
+
+            PointSet res{};
             // 尝试记录一个可能的 value
             auto join = [&](i32 val) -> void {
                 if (auto p = pointOf[val]; p != 0 and super[p]) {
-                    res[p] = true;
+                    res.set(p);
                 }
             };
 
-            if (block - extraBlock <= 1) {
+            if (extraBlock - totalBlock <= 1) {
                 // 全都是散块
                 for (i32 val = min; val <= max; val++) join(val);
                 return res;
             } else {
                 // 对于其中的所有整块，直接通过前缀和统计
-                // [size * extraBlock, size * (block - 1))
-                res = super & (blocks[block - 1].data ^ blocks[extraBlock].data);
+                // [size * totalBlock, size * extraBlock)
+                res = super & (blocks[totalBlock].owned ^ blocks[extraBlock].owned);
+                debug std::cerr << "res = " << res << std::endl;
 
-                debug std::cerr << res << std::endl;
                 // 散块暴力加入
-                // [size * (block - 1), max]
-                assert(max >= blockSize * (block - 1));
-                for (i32 val = blockEnd(block - 1); val <= max; val++) join(val);
-                // [min, size * extraBlock)
-                debug std::cerr << res << std::endl;
-                assert(min <= blockSize * extraBlock);
-                for (i32 val = min; val != blockEnd(extraBlock); val++) join(val);
-                debug std::cerr << res << std::endl;
+                // [size * extraBlock, max]
+                assert(max >= blockSize * extraBlock);
+                for (i32 val = blockBegin(extraBlock); val <= max; val++) join(val);
+                debug std::cerr << "res = " << res << std::endl;
+
+                // [min, size * totalBlock)
+                assert(min <= blockSize * totalBlock);
+                for (i32 val = min; val != blockBegin(totalBlock); val++) join(val);
+                debug std::cerr << "res = " << res << std::endl;
                 return res;
             }
         }
 
+#if false
         // 获取一个点集内权值最大的点
-        auto getMaxValue(PointsSet const &points) -> i32 {
-            i32 ans = 0;
-            for (i32 u = 1; u <= n; u++) {
-                if (points[u]) chkMax(ans, valueOf[u]);
+        auto getMaxValue(PointSet const &points) -> i32 {
+            // 寻找第一个和 points 没有交集的块
+            auto firstNoIntersect = lowerBoundValue(0, blockCount, true, [&](i32 i) -> bool {
+                return (blocks[i].owned & points).none();
+            });
+            if (firstNoIntersect == 0) return 0;
+            auto maxBlock = firstNoIntersect - 1;  // 最大值所处的块
+            for (i32 val = std::min(n + 1, blockBegin(maxBlock + 1)); val --> blockBegin(maxBlock); ) {
+                if (auto p = pointOf[val]; p != 0 and blocks[maxBlock].owned[p] and points[p]) {
+                    return val;
+                }
+            }
+            assert(false);
+            return 0;
+        }
+#else
+        auto getMaxValue(PointSet const &points) const -> i32 {
+            // 双指针
+            auto pointsPtr = points.data.begin();
+            auto valuesPtr = blocks.begin();
+
+            {
+                // Timer _{"双指针"};
+                for (; valuesPtr != blocks.end() and pointsPtr != points.data.end(); ++valuesPtr) {
+                    auto unmatch = [&](auto pointsPtr) -> bool {
+                        auto index = pointsPtr - points.data.begin();
+                        // std::cerr << "unmatch returns " << ((valuesPtr->owned.data[index] & *pointsPtr) == 0) << std::endl;
+                        return (valuesPtr->owned.data[index] & *pointsPtr) == 0;  // 存在交集即可
+                    };
+                    // 如果和当前点没有匹配上，就跳到下一组点
+                    while (pointsPtr != points.data.end() and unmatch(pointsPtr)) ++pointsPtr;
+                    if (pointsPtr == points.data.end()) break;  // 找到答案，直接跳出
+                }
             }
 
-            return ans;
+            auto firstNoIntersect = static_cast<i32>(valuesPtr - blocks.begin());
+            if (firstNoIntersect == 0) return 0;
+
+            {
+                // Timer _{"统计最大值"};
+                auto maxBlock = firstNoIntersect - 1;
+                // std::cerr << std::min(n + 1, blockBegin(maxBlock + 1)) - blockBegin(maxBlock) << std::endl;
+                for (i32 val = std::min(n + 1, blockBegin(maxBlock + 1)); val --> blockBegin(maxBlock); ) {
+                    if (auto p = pointOf[val]; p != 0 and blocks[maxBlock].owned[p] and points[p]) {
+                        return val;
+                    }
+                }
+            }
+            assert(false);
+            return 0;
         }
+#endif
     };
 #endif
 
     auto solve() -> void {
+        // std::cerr << "qwq" << std::endl << std::flush;
+        // Timer::clearTable();
         i32 n, m, q;
         std::cin >> n >> m >> q;
 
@@ -281,7 +471,7 @@ namespace Solution_1227247956830917 {
         for (auto &x: limits | views::drop(1)) std::cin >> x;
         for (auto &x: values | views::drop(1)) std::cin >> x;
 
-        Blocks bLim(n), bVal(n);
+        Blocks bLim(n, static_cast<i32>(std::sqrt(n))), bVal(n, static_cast<i32>(std::sqrt(n)));
         for (i32 u = 1; u <= n; u++) {
             bLim.insert(u, limits[u]);
             bVal.insert(u, values[u]);
@@ -312,6 +502,8 @@ namespace Solution_1227247956830917 {
                 i32 u, l, r;
                 std::cin >> u >> l >> r;
 
+                // Timer _{"Query"};
+
                 // 获取被选中的点集
                 auto selected = bLim.getPointsBetween(l, r, reach[u]);
                 debug std::cerr << "selected: " << selected << std::endl;
@@ -319,6 +511,11 @@ namespace Solution_1227247956830917 {
                 std::cout << ans << endl;
             }
         }
+
+        // for (auto const &[key, time]: Timer::getTable()) {
+        //     std::cerr << key << ": " << chrono::duration_cast<chrono::milliseconds>(time).count() << "ms" << std::endl;
+        // }
+        // std::cerr << "123" << std::endl << std::flush;
     }
 }
 
--- test.cpp
+++ test.cpp
@@ -1,82 +1,218 @@
-/**
- * @link
- */
-#include "./lib_v7.hpp"
-#include "./libs/fixed_int.hpp"
-using namespace lib;
+#include<bits/stdc++.h>
+using namespace std;
+using ull=unsigned long long;
+const int N=1e3+5;
+const int sq=340;
+const int siz=1570;
+struct bit{
+	ull a[siz];
+	bit() {
+		reset();
+	}
+	void reset(){
+		memset(a,0,sizeof(a));
+	}
+	bool get(int x) const {
+		return a[x>>6]>>(x&63)&1;
+	}
+	void set1(int x){
+		a[x>>6]|=1ull<<(x&63);
+	}
+	void set0(int x){
+		a[x>>6]&=~(1ull<<(x&63));
+	}
+	void operator |=(const bit &b){
+		for(int i=0;i<siz;i++)a[i]|=b.a[i];
+	}
+	void operator ^=(const bit &b){
+		for(int i=0;i<siz;i++)a[i]^=b.a[i];
+	}
+	void operator &=(const bit &b){
+		for(int i=0;i<siz;i++)a[i]&=b.a[i];
+	}
+	auto operator& (const bit &other) const -> bit {
+		auto tmp = *this;
+		return tmp &= other, tmp;
+	}
+};
+vector<int> mp[N];
+bit rch[N];
+bit sa[sq+2],sb[sq+2];
 
-namespace Solution_6314180276493067 {
-    auto solve() -> void {
-        std::string s, match;
-        std::cin >> s >> match;
-
-        s.insert(s.begin(), '_');
-        match.insert(match.begin(), '_');
-        // s += '_', match += '_';
-
-        auto n = static_cast<i32>(s.size()) - 1, m = static_cast<i32>(match.size()) - 1;
-
-        std::vector<i32> next(m + 1);
-#if false
-        for (i32 len = 1; len <= m; len++) {
-            // 暴力匹配前后缀
-            i32 i = 1, j = len;
-            for (auto _ = len; _ --> 0; ) {
-                if (match.substr(1, i - 1) == match.substr(j + 1, len - j)) {
-                    next[len] = i - 1;
-                }
-                i++, j--;
-            }
-        }
-#else
-        i32 curMax = 0;  // 当前最长前缀的结尾
-        for (i32 i = 1; i < m; i++) {
-            while (curMax > 0 and match[curMax + 1] != match[i + 1]) {
-                curMax = next[curMax];
-            }
-            if (match[curMax + 1] == match[i + 1]) {
-                curMax++;
-            }
-            next[i + 1] = curMax;
-        }
-#endif
-        debug {
-            std::cout << "next: " << std::endl;
-            for (i32 i = 1; i <= m; i++) {
-                std::cout << "[" << i << "] " << next[i] << "  ";
-            }
-            std::cout << std::endl;
-        }
-
-        auto find = [&]() -> void {
-            i32 i = 0, j = 0;
-
-            for (; i <= n; i++) {
-                while (j > 0 and s[i + 1] != match[j + 1]) {
-                    j = next[j];
-                }
-                if (s[i + 1] == match[j + 1]) {
-                    j++;
-                }
-                if (j == m) {
-                    std::cout << i - m + 2 << endl;
-                }
-            }
-        };
-        find();
-
-        for (i32 i = 1; i <= m; i++) {
-            std::cout << next[i] << " ";
-        }
-        std::cout << endl;
-    }
+int a[N],b[N];
+int ra[N],rb[N];
+int n,m,q;
+int st[N],ed[N],bl[N];
+void build(){
+	for(int i=1;i<=sq;i++){
+		st[i]=n/sq*(i-1)+1;
+		ed[i]=n/sq*i;
+	}
+	ed[sq]=n;
+	for(int i=1;i<=sq;i++){
+		for(int j=st[i];j<=ed[i];j++){
+			bl[j]=i;
+		}
+	}
 }
+void init(){
+	for(int i=sq;i>=1;i--){
+		sa[i]|=sa[i+1];
+		sb[i]|=sb[i+1];
+		for(int j=st[i];j<=ed[i];j++){
+			sa[i].set1(ra[j]);
+			sb[i].set1(rb[j]);
+		}
+	}
+}
+void swapA(int l,int r){
+	if(a[l]>a[r])swap(l,r);
+	for(int i=bl[a[l]]+1;i<=bl[a[r]];i++){
+		sa[i].set1(l);
+		sa[i].set0(r);
+	}
+	swap(ra[a[l]],ra[a[r]]);
+	swap(a[l],a[r]);
+}
+void swapB(int l,int r){
+	if(b[l]>b[r])swap(l,r);
+	for(int i=bl[b[l]]+1;i<=bl[b[r]];i++){
+		sb[i].set1(l);
+		sb[i].set0(r);
+	}
+	swap(rb[b[l]],rb[b[r]]);
+	swap(b[l],b[r]);
+}
+bit gets(int x,int l,int r){
+	bit res;
+	if(bl[l]==bl[r]){
+		for(int i=l;i<=r;i++){
+			res.set1(ra[i]);
+		}
+	}else{
+		res|=sa[bl[l]+1];
+		res^=sa[bl[r]];
+		for(int i=l;i<=ed[bl[l]];i++){
+			res.set1(ra[i]);
+		}
+		for(int i=st[bl[r]];i<=r;i++){
+			res.set1(ra[i]);
+		}
+	}
+	res&=rch[x];
+	return res;
+}
+int query(int x,int l,int r){
+	bit des=gets(x,l,r);
+	int i=1,j=0;
+//	for(;i<=sq&&j<siz;){
+//		if(sb[i].a[j]&des.a[j])i++;
+//		else j++;
+//	}
+	for (; i <= n and j != siz; ++i) {
+		auto unmatch = [&](int j) -> bool {
+			return (sb[i].a[j] & des.a[j]) == 0;
+		};
+		while (j != siz and unmatch(j)) ++j;
+		if (j == siz) break;
+		
+		cerr << "i = " << i << ", j = " << j << endl;
+		{
+			cerr << "match " << sb[i].a[j] << "with" << des.a[j] << endl;
+			cerr << "x & y: " << (sb[i].a[j] & des.a[j]) << endl;
+			auto tmp = des & sb[i];
+			for (int i = 1; i <= n; i++) {
+//				cerr << tmp.get(i);
+				if (tmp.get(i)) cerr << i << "(" << bl[b[i]] << ")" << ", ";
+			}
+			cerr << endl;
+		}
+	}
+	i--;
+	if (i == 0) return 0;
 
-auto main(int argc, char const *argv[]) -> int {
-    DEBUG_MODE = (argc != 1) and (std::strcmp("-d", argv[1]) == 0);
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr), std::cout.tie(nullptr);
-
-    Solution_6314180276493067::solve();
-    return 0;
+	for(j=ed[i];j>=st[i];j--){
+		if(des.get(rb[j]))return j;
+	}
+	
+	
+	cerr << "i = " << i << endl;
+	{
+		auto tmp = des & sb[i];
+		for (int i = 1; i < n + 1; i++) {
+//				cerr << tmp.get(i);
+			if (tmp.get(i)) cerr << i << "(" << bl[b[i]] << ")" << ", ";
+		}
+		cerr << endl;
+	}
+	cerr << "ed[i] = " << ed[i] << ", st[i] = " << st[i] << endl;
+	assert(false);
+	return 0;
+}
+void solve(){
+	cin>>n>>m>>q;
+	for(int i=1;i<=n;i++)mp[i].clear();
+	for(int i=1;i<=m;i++){
+		int u,v;
+		cin>>u>>v;
+		mp[u].push_back(v);
+	}
+	for(int i=1;i<=n;i++){
+		rch[i].reset();
+		rch[i].set1(i);
+	}
+	for(int i=n;i>=1;i--){
+		for(auto j:mp[i]){
+			rch[i]|=rch[j];
+		}
+	}
+	for(int i=1;i<=n;i++){
+		cin>>a[i];
+		ra[a[i]]=i;
+	}
+	for(int i=1;i<=n;i++){
+		cin>>b[i];
+		rb[b[i]]=i;
+	}
+	build();
+	init();
+	
+	// for (int j = sq; j <= sq; j++) {
+	// 	{
+	// 		auto tmp = sb[j];
+	// 		cerr << "j = " << j << endl;
+	// 		for (int i = 1; i < n + 1; i++) {
+	// //				cerr << tmp.get(i);
+	// 			if (tmp.get(i)) cerr << i << "(b[i] = " << b[i] << ", bl[b[i]] = " << bl[b[i]] << ")" << ", ";
+	// 		}
+	// 		cerr << endl;
+	// 	}
+	// }
+	// return;
+	
+	for(int i=1;i<=q;i++){
+		int op,x,y,z;
+		cin>>op>>x>>y;
+		if(op==1){
+			swapA(x,y);
+		}
+		if(op==2){
+			swapB(x,y);
+		}
+		if(op==3){
+			cin>>z;
+			cout<<query(x,y,z)<<"\n";
+		}
+	}
+	
+}
+int main(){
+	freopen("t1.in", "r", stdin);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int c,T;
+	cin>>c>>T;
+	while(T--){
+		solve();
+	}
 }
